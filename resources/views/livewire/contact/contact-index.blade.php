3463bc968b0fada0b2ac109ed84adbbc03c901fd
nilakone <sivilay.nilakone@gmail.com>
2022-06-30 12:49:28 +0700

    contact page responsive

--- resources/views/livewire/contact/contact-index.blade.php
+++ resources/views/livewire/contact/contact-index.blade.php
@@ -9,24 +9,24 @@
                 <h1>ຕິດຕໍ່ພົວພັນ</h1>
             </article>
 
-            <section class="flex gap-12 card-actions mt-4">
-                <div class=" bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.6923641897306!2d102.61051600979036!3d17.962831637565557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3124692973fc1a77%3A0xfe40723db9ff6ff3!2sMinistry%20of%20education%20and%20Sport%20(Sisaketh%20Area)!5e0!3m2!1sen!2sla!4v1656500569316!5m2!1sen!2sla" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <section class="flex gap-12 card-actions mt-4 flex-col lg:flex-row">
+                <div class="flex-1 w-full bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.6923641897306!2d102.61051600979036!3d17.962831637565557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3124692973fc1a77%3A0xfe40723db9ff6ff3!2sMinistry%20of%20education%20and%20Sport%20(Sisaketh%20Area)!5e0!3m2!1sen!2sla!4v1656500569316!5m2!1sen!2sla" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
-                <div class=" bg-white rounded-lg">
-                    <div class="grid grid-cols-3 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-1">
-                        <div class="flex gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
+                <div class="flex-1 bg-white rounded-lg">
+                    <div class="flex flex-col gap-6 mt-6 sm:grid-cols-2 md:grid-cols-1">
+                        <div class="flex flex-1 gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
                             <ion-icon class="w-10 h-10" name="location-sharp"></ion-icon>
                             <div class="mt-2"><h2>ສະຖານທີ່</h2><p>ຖະໜົມເສດຖາທິລາດ, ບ້ານຊຽງຢືນ, ເມື່ອງຈັນທະບູລີ, ນະຄອນຫຼວງວຽງຈັນ, ສ.ປ.ປ.ລາວ.</p></div>
                         </div>
 
-                        <div class="flex gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
+                        <div class="flex flex-1 gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
                             <ion-icon class="w-10 h-10" name="call-sharp"></ion-icon>
                             <div class="mt-2"><h2>ເບີໂທຕິດຕໍ່</h2><p>+856 20 28 240 541</p></div>
                         </div>
 
-                        <div class="flex gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
+                        <div class="flex flex-1 gap-5 items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-20">
                             <ion-icon class="w-10 h-10" name="mail-sharp"></ion-icon>
                             <div class="mt-2"><h2>ອີເມວ</h2><p>hrd@gmail.com</p></div>
                         </div>
